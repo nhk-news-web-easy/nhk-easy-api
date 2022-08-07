0e8dc93daf13032aa1f729da2166af692daa209e
Frederick-S <mao_xiaodan@hotmail.com>
2022-08-07 11:12:35 +0800

    news: query news within a month

--- src/main/kotlin/nhk/controller/api/NewsController.kt
+++ src/main/kotlin/nhk/controller/api/NewsController.kt
@@ -5,22 +5,34 @@ import nhk.dto.NewsDto
 import nhk.repository.NewsRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.format.annotation.DateTimeFormat
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
-import java.util.Date
+import java.util.*
 
 @RestController
 class NewsController : BaseController() {
     @Autowired
     private lateinit var newsRepository: NewsRepository
 
+    private val thirtyDaysInMilliseconds = 30 * 24 * 60 * 60 * 1000L
+
     @GetMapping("/news")
     fun getNews(
         @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) startDate: Date,
         @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) endDate: Date
-    ): List<NewsDto> {
-        return newsRepository.findByPublishedAtUtcBetween(startDate.toInstant(), endDate.toInstant())
+    ): ResponseEntity<List<NewsDto>> {
+        if (endDate.before(startDate)) {
+            return ResponseEntity(null, HttpStatus.BAD_REQUEST)
+        }
+
+        if (endDate.time - startDate.time > thirtyDaysInMilliseconds) {
+            return ResponseEntity(null, HttpStatus.BAD_REQUEST)
+        }
+
+        val newsList = newsRepository.findByPublishedAtUtcBetween(startDate.toInstant(), endDate.toInstant())
             .map { news ->
                 val dto = NewsDto()
                 dto.newsId = news.newsId
@@ -36,5 +48,7 @@ class NewsController : BaseController() {
 
                 dto
             }
+
+        return ResponseEntity(newsList, HttpStatus.OK)
     }
 }
